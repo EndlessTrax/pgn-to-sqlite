a2f5bacda4bdfdc7b0d1bdaa441b8d2be5c453a4
endlesstrax <ricky@whitelionmedia.com>
2021-02-24 16:22:55 -0500

    Deleted conftest, no longer contains used code.

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import requests
-
-
-@pytest.fixture()
-def example_pgn() -> str:
-    r = requests.get("https://api.chess.com/pub/player/endlesstrax/games/2021/02")
-    return r.json()["games"][5]["pgn"]
