46a94766dca6dac82de01170946564873e4a8aa4
endlesstrax <ricky@whitelionmedia.com>
2021-04-14 16:26:13 -0400

    Added tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
-from pgn_to_sqlite.cli import convert_to_snake_case, build_pgn_dict
+import requests
+from click.testing import CliRunner
+from pgn_to_sqlite.cli import build_pgn_dict, convert_to_snake_case, main
 
 
 def test_snake_case_conversion():
     result = convert_to_snake_case("SpamSpam")
     assert result == "spam_spam"
+
 
 def test_build_png_dict():
     with open("tests/test_pgn_file.pgn", "r") as f:
@@ -12,3 +15,16 @@ def test_build_png_dict():
 
     assert result["white"] == "EndlessTrax"
     assert result["termination"] == "EndlessTrax won by checkmate"
+
+
+def test_chess_dotcom_api_endpoint():
+    r = requests.get(f"https://api.chess.com/pub/player/endlesstrax/games/archives")
+    assert r.status_code == 200
+
+
+def test_ValueError_on_invalid_args():
+    runner = CliRunner()
+    result = runner.invoke(
+        main, ["invalid_site", "-u", "endlesstrax", "-o", "games.db"]
+    )
+    assert result.exit_code == 1
